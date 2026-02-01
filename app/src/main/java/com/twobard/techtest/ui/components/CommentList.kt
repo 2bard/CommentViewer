c1de0332b4467a8c9a2d3630a984ea7f26c0490e
2bard <lewisandrewbaker@gmail.com>
2026-02-01 20:37:11 +0000

    Update PreviewParamProviders

--- app/src/main/java/com/twobard/techtest/ui/components/CommentList.kt
+++ app/src/main/java/com/twobard/techtest/ui/components/CommentList.kt
@@ -11,8 +11,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import com.twobard.techtest.domain.repository.Comment
-import com.twobard.techtest.ui.list.CommentPreviewProvider
 import com.twobard.techtest.ui.theme.ThemePadding
 
 
@@ -31,4 +31,22 @@ fun ListItem(
             Text(text = comment.body,  style = MaterialTheme.typography.bodyMedium)
         }
     }
+}
+
+class CommentListPreviewProvider : PreviewParameterProvider<List<Comment>> {
+    override val values = sequenceOf(
+        emptyList(),
+        listOf(
+            Comment(postId = 1, id = 1, name = "C Comment", email = "test@test.com", body = "Hello 1"),
+            Comment(postId = 2, id = 2, name = "A Comment", email = "test2@test.com", body = "Hello 2"),
+            Comment(postId = 3, id = 3, name = "B Comment", email = "test3@test.com", body = "Hello 3")
+        )
+    )
+}
+
+class CommentPreviewProvider : PreviewParameterProvider<Comment> {
+    override val values = sequenceOf(
+        Comment(postId = 1, id = 1, name = "Normal name", email = "test@test.com", body = "Normal body comment"),
+        Comment(postId = 1, id = 1, name = "Long name. Long name. Long name. Long name. Long name. Long name.  ", email = "test@test.com", body = "Long comment. Long comment. Long comment. Long comment. Long comment. Long comment. Long comment. "),
+    )
 }
