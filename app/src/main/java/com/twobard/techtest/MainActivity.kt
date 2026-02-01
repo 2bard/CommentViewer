0095b3f083835991a3e039efe7678a697a356f03
2bard <lewisandrewbaker@gmail.com>
2026-02-01 20:36:47 +0000

    Modify ListScreen - hoist click listeners into nav component

--- app/src/main/java/com/twobard/techtest/MainActivity.kt
+++ app/src/main/java/com/twobard/techtest/MainActivity.kt
@@ -4,22 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.collectAsState
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.twobard.techtest.domain.repository.Comment
 import com.twobard.techtest.ui.detail.DetailScreen
 import com.twobard.techtest.ui.list.CommentListViewModel
 import com.twobard.techtest.ui.list.ListScreen
 import com.twobard.techtest.ui.theme.TechTestTheme
 import dagger.hilt.android.AndroidEntryPoint
+import androidx.compose.runtime.getValue
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -39,13 +37,19 @@ fun TechTestAppComposable() {
     val navController = rememberNavController()
     val startRoute = "home"
     NavHost(navController, startDestination = startRoute) {
+
+        //List screen
         composable("home") { backStackEntry ->
             val viewModel = hiltViewModel<CommentListViewModel>()
-            ListScreen(viewModel) {
+            val comments by viewModel.comments.collectAsState(initial = listOf())
+            val onClickItem: ((Comment) -> Unit) = { navController.navigate("detail") }
+            val onClickRefresh : (() -> Unit) = { viewModel.loadComments() }
+            ListScreen(comments, onClickItem) {
                 navController.navigate("detail")
             }
         }
 
+        //Detail screen
         composable("detail") { backStackEntry ->
             DetailScreen() {
                 navController.popBackStack()
--- app/src/main/java/com/twobard/techtest/ui/list/ListScreen.kt
+++ app/src/main/java/com/twobard/techtest/ui/list/ListScreen.kt
@@ -1,113 +1,93 @@
 package com.twobard.techtest.ui.list
 
+import ListItem
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.stylusHoverIcon
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.twobard.techtest.R
 import com.twobard.techtest.domain.repository.Comment
+import com.twobard.techtest.ui.components.EmptyListState
+import com.twobard.techtest.ui.components.NavBarText
 import com.twobard.techtest.ui.theme.ThemePadding
-import kotlin.collections.listOf
 
 
 @OptIn(ExperimentalMaterial3Api::class)
+@Preview
 @Composable
 fun ListScreen(
-    viewModel: CommentListViewModel = viewModel(),
-    onClickItem: () -> Unit = {}
+    @PreviewParameter(CommentListPreviewProvider::class) items: List<Comment>,
+    onClickItem: (Comment) -> Unit = {},
+    onClickReload: () -> Unit = {}
 ) {
-
-    val items by viewModel.comments.collectAsState(initial = emptyList())
 
     Scaffold(
         topBar =  {
             TopAppBar(
-                title = { Text(stringResource(R.string.comment_list)) }
+                title = {
+                    NavBarText(stringResource(R.string.comment_list))
+                }
             )
         }
     ) {
         paddingValues ->
         Box(modifier = Modifier.padding(paddingValues)) {
-            Box(modifier = Modifier.clickable{
-                onClickItem()
-            }) {
-                Text("test123")
-            }
+            ItemList(
+                values = items,
+                onClickItem = onClickItem,
+                onClickReload = onClickReload
+            )
         }
     }
 
-}
-
-class CommentPreviewProvider : PreviewParameterProvider<List<Comment>> {
-    override val values = sequenceOf(
-        emptyList(),
-        listOf(
-            Comment(postId = 1, id = 1, name = "C Comment", email = "test@test.com", body = "Hello 1"),
-            Comment(postId = 2, id = 2, name = "A Comment", email = "test2@test.com", body = "Hello 2"),
-            Comment(postId = 3, id = 3, name = "B Comment", email = "test3@test.com", body = "Hello 3")
-        )
-    )
 }
 
 @Preview
 @Composable
 fun ItemList(
-    @PreviewParameter(CommentPreviewProvider::class) values: List<Comment>
+    @PreviewParameter(CommentListPreviewProvider::class) values: List<Comment>,
+    onClickItem: (Comment) -> Unit = {},
+    onClickReload: () -> Unit = {}
 ) {
-    if(values.isEmpty()){
-        EmptyListState()
-    } else {
-        LazyColumn(verticalArrangement = Arrangement.spacedBy(ThemePadding().listPadding())) {
-            items(values.size) {
-                Text(
-                    text = values[it].name
-                )
+    Box(modifier = Modifier.padding(ThemePadding().screenPadding())){
+        if(values.isEmpty()){
+            EmptyListState(onClickReload)
+        } else {
+            LazyColumn(verticalArrangement = Arrangement.spacedBy(ThemePadding().listPadding())) {
+                items(values.size) {
+                    ListItem(comment = values[it], onClickItem = onClickItem)
+                }
             }
         }
     }
 }
 
-@Composable
-fun EmptyListState(onClickReload: () -> Unit = {}) {
-    Column(modifier = Modifier.padding(ThemePadding().boxPadding())) {
-        Text(text = "No comments available")
 
-        Button(onClick = {
-            onClickReload()
-        }) {
-            Row {
 
-                Icon(
-                    imageVector = Icons.Default.Refresh,
-                    contentDescription = stringResource(R.string.refresh)
-                )
 
-                Spacer(modifier = Modifier.width(ThemePadding().boxPadding()))
 
-                Text(stringResource(R.string.refresh))
-            }
-        }
-    }
-}
