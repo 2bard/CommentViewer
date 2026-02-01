60a54ede90e7464792fa04169de9b1530adbb131
2bard <lewisandrewbaker@gmail.com>
2026-02-01 22:32:31 +0000

    Move errors into BaseViewModel so they can be reused. Add logging to BaseViewModel

--- app/src/main/java/com/twobard/techtest/ui/BaseViewModel.kt
+++ app/src/main/java/com/twobard/techtest/ui/BaseViewModel.kt
@@ -1,4 +1,25 @@
 package com.twobard.techtest.ui
 
-class BaseViewModel {
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.twobard.techtest.data.NetworkError
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+
+abstract class BaseViewModel : ViewModel() {
+
+    private val _errors = MutableSharedFlow<NetworkError?>()
+    val errors: SharedFlow<NetworkError?> = _errors
+
+    suspend fun handleError(e: NetworkError?){
+        _errors.emit(e)
+        log("Network Error", e)
+    }
+
+    abstract fun getTag(): String
+
+    fun log(msg: String, e: Throwable?){
+        //Note: Would usually call Timber here, or some injected log class
+        Log.e(getTag(), msg, e)
+    }
 }
