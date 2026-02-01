066dd0ae4ee3bffb6fc4e33f34e99418bcc76668
2bard <lewisandrewbaker@gmail.com>
2026-02-01 21:00:33 +0000

    Implement DetailScreen ui

--- app/src/main/java/com/twobard/techtest/ui/detail/DetailScreen.kt
+++ app/src/main/java/com/twobard/techtest/ui/detail/DetailScreen.kt
@@ -1,20 +1,38 @@
 package com.twobard.techtest.ui.detail
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
 import com.twobard.techtest.R
+import com.twobard.techtest.domain.repository.Comment
+import com.twobard.techtest.ui.components.NavBarText
+import com.twobard.techtest.ui.theme.ThemePadding
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -25,9 +43,8 @@ fun DetailScreen(
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(stringResource(R.string.comment_detail)) },
+                title = { NavBarText(stringResource(R.string.comment_detail)) },
                 navigationIcon = {
-
                     IconButton(
                         onClick = { onClickBack() }
                     ) {
@@ -36,11 +53,6 @@ fun DetailScreen(
                             contentDescription = stringResource(R.string.back)
                         )
                     }
-                    Text(
-                        text = stringResource(R.string.back),
-                        modifier = Modifier
-                            .clickable { onClickBack() }
-                    )
                 }
             )
         }
@@ -53,5 +65,46 @@ fun DetailScreen(
             }
         }
     }
+}
 
+@Composable
+@Preview
+fun DetailPanel(@PreviewParameter(CommentPreviewProvider::class) comment: Comment){
+
+    Card {
+        Column {
+            Column(modifier = Modifier
+                .padding(ThemePadding.boxPadding())
+                .fillMaxWidth(), verticalArrangement = Arrangement.spacedBy(ThemePadding.listPadding())) {
+                Text(text = comment.name, style = MaterialTheme.typography.titleMedium)
+                Text(text = comment.body,  style = MaterialTheme.typography.bodySmall)
+            }
+
+            CommentPropertyDivider()
+            Column {
+                PropertyRow(stringResource(R.string.id), comment.id.toString())
+                CommentPropertyDivider()
+                PropertyRow(stringResource(R.string.email), comment.email)
+            }
+        }
+
+    }
+}
+
+@Composable
+fun PropertyRow(key: String, value: String){
+
+    Row(modifier = Modifier.padding(ThemePadding.boxPadding())){
+        Text(modifier = Modifier.wrapContentSize(), text = key, style = MaterialTheme.typography.bodyMedium)
+        Text(modifier = Modifier.weight(1f), maxLines = 1, text = value, style = MaterialTheme.typography.bodyMedium, textAlign = TextAlign.End, overflow = TextOverflow.Ellipsis)
+    }
+}
+
+@Composable
+fun CommentPropertyDivider(){
+    HorizontalDivider(
+        modifier = Modifier.fillMaxWidth(),
+        thickness = ThemePadding.dividerThickness(),
+        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f)
+    )
 }
--- app/src/main/java/com/twobard/techtest/ui/theme/ThemePadding.kt
+++ app/src/main/java/com/twobard/techtest/ui/theme/ThemePadding.kt
@@ -3,6 +3,13 @@ package com.twobard.techtest.ui.theme
 import androidx.compose.ui.unit.dp
 
 class ThemePadding {
-    fun listPadding() = 16.dp
-    fun boxPadding() = 4.dp
+
+    companion object {
+        fun listPadding() = 16.dp
+        fun boxPadding() = 4.dp
+        fun screenPadding() = 12.dp
+        fun elementSpacing() = 8.dp
+        fun defaultElevation() = 4.dp
+        fun dividerThickness() = 1.dp
+    }
 }
