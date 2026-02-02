488524b8737221754c842c12673bec0643310195
2bard <lewisandrewbaker@gmail.com>
2026-02-02 21:04:20 +0000

    Add more UI tests

--- app/src/test/java/com/twobard/techtest/ui/DetailScreenPreviewTest.kt
+++ app/src/test/java/com/twobard/techtest/ui/DetailScreenPreviewTest.kt
@@ -1,27 +1,58 @@
 package com.twobard.techtest.ui
 
+import CommentListPreviewProvider
 import CommentPreviewProvider
+import app.cash.paparazzi.DeviceConfig
 import app.cash.paparazzi.Paparazzi
 import com.twobard.techtest.ui.detail.DetailScreenPreview
+import com.twobard.techtest.ui.list.CommentListScreen
+import com.twobard.techtest.ui.list.ListScreenIsLoadingPreview
 import com.twobard.techtest.ui.theme.TechTestTheme
 import org.junit.Rule
 import kotlin.test.Test
 
-class ComposePreviewTest {
+
+class DetailScreenPreviewTest {
 
     @get:Rule
-    val paparazzi = Paparazzi()
+    val paparazzi = Paparazzi(
+        deviceConfig = DeviceConfig.PIXEL_5
+    )
 
     @Test
-    fun myComposable() {
-
+    fun detailScreenPreview() {
         val provider = CommentPreviewProvider()
         provider.values.forEach {
-            paparazzi.snapshot(name = "UserCard_{${it.id}") {
+            paparazzi.snapshot(name = "DetailScreen_{${it.hashCode()}") {
                 TechTestTheme {
                     DetailScreenPreview(it)
                 }
             }
         }
     }
+
+    @Test
+    fun listScreenPreview() {
+        val provider = CommentListPreviewProvider()
+        provider.values.forEach {
+            paparazzi.snapshot(name = "DetailScreen_{${it.hashCode()}") {
+                TechTestTheme {
+                    CommentListScreen(it)
+                }
+            }
+        }
+    }
+
+    @Test
+    fun listScreenLoadingPreview() {
+        val provider = CommentListPreviewProvider()
+        provider.values.forEach {
+            paparazzi.snapshot(name = "LoadingDetailScreen_{${it.hashCode()}") {
+                TechTestTheme {
+                    ListScreenIsLoadingPreview()
+                }
+            }
+        }
+    }
+
 }
