9a088be50cefe9dc43e7fe31aa6a64b06adf7061
2bard <lewisandrewbaker@gmail.com>
2026-02-02 20:08:11 +0000

    Add GetCommentUseCaseTest

--- app/src/test/java/com/twobard/techtest/GetCommentUseCaseTest.kt
+++ app/src/test/java/com/twobard/techtest/GetCommentUseCaseTest.kt
@@ -2,6 +2,7 @@ package com.twobard.techtest
 
 import com.twobard.techtest.domain.repository.Comment
 import com.twobard.techtest.domain.repository.CommentRepository
+import com.twobard.techtest.domain.usecase.GetCommentUseCase
 import com.twobard.techtest.domain.usecase.SortedCommentsUseCase
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
@@ -9,30 +10,28 @@ import org.mockito.Mockito.mock
 import org.mockito.kotlin.whenever
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
 
-class FilteredCommentsUseCaseTest : BaseUseCaseTest() {
+class GetCommentUseCaseTest : BaseUseCaseTest() {
 
-    private lateinit var useCase: SortedCommentsUseCase
+    private lateinit var useCase: GetCommentUseCase
 
     @Before
     override fun setup() {
         super.setup()
-        useCase = SortedCommentsUseCase(repository)
+        useCase = GetCommentUseCase(repository)
     }
 
     @Test
-    fun `given a list of unordered Comments when UseCase invoked then Comments should be ordered alphabetically`() = runTest {
+    fun `given a list of unordered Comments when an ID is provided then return the Comment matching the ID`() = runTest {
 
         whenever(repository.getComments()).thenReturn(Result.success(testComments()))
 
-        val result = useCase.invoke().getOrThrow()
+        val result = useCase.invoke(1).getOrThrow()
 
-        assertEquals(3, result.size)
-
-        //Check sort order, should be A, B, C
-        assertEquals("A Comment", result[0].name)
-        assertEquals("B Comment", result[1].name)
-        assertEquals("C Comment", result[2].name)
+        assertNotNull(result)
+        assertEquals(1, result.id)
+        assertEquals("C Comment", result.name)
     }
 }
