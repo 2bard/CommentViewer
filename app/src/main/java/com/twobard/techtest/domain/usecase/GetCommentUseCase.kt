3489535c5e89fd7def64cafbe97014001316ac7f
2bard <lewisandrewbaker@gmail.com>
2026-02-02 19:45:47 +0000

    Implement getting comment by ID in UseCase

--- app/src/main/java/com/twobard/techtest/domain/usecase/GetCommentUseCase.kt
+++ app/src/main/java/com/twobard/techtest/domain/usecase/GetCommentUseCase.kt
@@ -4,14 +4,14 @@ import com.twobard.techtest.domain.repository.Comment
 import com.twobard.techtest.domain.repository.CommentRepository
 import javax.inject.Inject
 
-class SortedCommentsUseCase @Inject constructor(
+class GetCommentUseCase @Inject constructor(
     private val repository: CommentRepository
 ) {
 
-    suspend operator fun invoke(): Result<List<Comment>> {
-        val comments = repository.getComments().map {
-            it.sortedBy { it.name } //Sorting by name - spec says 'alphabetical order' - assuming this is name
+    suspend operator fun invoke(id: Int): Result<Comment?> {
+        val comment = repository.getComments().map {
+            it.first { it.id == id }
         }
-        return comments
+        return comment
     }
 }
